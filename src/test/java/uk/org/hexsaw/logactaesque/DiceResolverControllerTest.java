56779f10d5f177f415a09ec537c2064549c0f4fe
Steve Westwood <steve.westwood@hexsaw.org.uk>
2020-10-03 22:35:46 +0100

    Tidy imports

--- src/test/java/uk/org/hexsaw/logactaesque/DiceResolverControllerTest.java
+++ src/test/java/uk/org/hexsaw/logactaesque/DiceResolverControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.hamcrest.MatcherAssert.assertThat; 
